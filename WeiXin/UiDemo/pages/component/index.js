c574bc7326472a6ea6176b1aa4f6f25b49c26f5c
YC-YC <yellowc.yc@aliyun.com>
2018-06-04 17:42:57 +0800

    基本小程序

---
+++ WeiXin/UiDemo/pages/component/index.js
@@ -0,0 +1,33 @@
+// pages/component/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [{
+      id:"view",
+      name:"视图容器",
+      open:false,
+      pages:['view', 'scroll-vew', 'swiper']
+    },
+    {
+      id: "content",
+      name: "基础内容",
+      open: false,
+      pages: ['text', 'icon', 'progress']
+    }]
+  },
+
+  kinkToggle: function(e){
+    var id = e.currentTarget.id, list = this.data.list;
+    for( var i = 0, len = list.length; i < len; i++){
+      if (list[i].id == id){
+        list[i].open = !list[i].open;
+      }
+      else{
+        list[i].open = false;
+      }
+    }
+  }
+})
